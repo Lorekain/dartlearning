07b491e7b25b227337f5ba2290ef53e2ead43d9a
psyhh <abarabash.it@gmail.com>
2024-10-01 19:47:21 +0300

    http learning

--- dart_learning/bin/dart_learning.dart
+++ dart_learning/bin/dart_learning.dart
@@ -1,23 +1,45 @@
-
-class Book {
-double price = 0;
-String? author;
-int pages = 0;
+import 'dart:io';
+import 'package:http/http.dart' as http; // http request and responses
+import 'dart:convert'; // json
 
 
+const String token = '7331024389:AAEQENPYA49rzDZhZITWkmOTLH-9yUeUv7o';
 
-Book(this.price, this.author, this.pages);
-@override String toString() {
-    // TODO: implement toString
-    return '$price, $author, $pages';
-  }
-}
+void main() async {
+  final urlSendMessage = Uri.parse('https://api.telegram.org/bot$token/sendMessage');
+  final urlGetUpdates = Uri.parse('https://api.telegram.org/bot$token/getUpdates');
+    final sendMessage = await http.post(urlSendMessage,body: {
+        'chat_id' : '612763187',
+        'text' : '*privet*',
+    
+    });
+  final getUpdates = await http.get(urlGetUpdates);
 
 
 
-void main() {
-  Book designTheory = Book(15, "chel", 200);
 
-  print(designTheory);
+
+
+
+   if (sendMessage.statusCode == 200) {
+      print('message  delivered');
+      
+   }
+     else { 
+      print ('no');
+     }
+
+     if (getUpdates.statusCode == 200) {
+          final dynamic data = jsonEncode(getUpdates.body);
+          
+            final message = data['message'];
+            final text = data['text'];
+        await http.post(urlSendMessage, body: {
+          'chat_id' : '612763187',
+          'text' : text,
+        });
+          
+    }
+
 
 }
