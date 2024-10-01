0fa9ad1d5768a77f57c2fce5afa6c6a28344b49c
psyhh <abarabash.it@gmail.com>
2024-10-01 20:20:33 +0300

    before sendMessage func

--- dart_learning/bin/dart_learning.dart
+++ dart_learning/bin/dart_learning.dart
@@ -5,41 +5,52 @@ import 'dart:convert'; // json
 
 const String token = '7331024389:AAEQENPYA49rzDZhZITWkmOTLH-9yUeUv7o';
 
+
+
 void main() async {
-  final urlSendMessage = Uri.parse('https://api.telegram.org/bot$token/sendMessage');
-  final urlGetUpdates = Uri.parse('https://api.telegram.org/bot$token/getUpdates');
-    final sendMessage = await http.post(urlSendMessage,body: {
-        'chat_id' : '612763187',
-        'text' : '*privet*',
-    
-    });
-  final getUpdates = await http.get(urlGetUpdates);
+  final sendMessageUrl = Uri.parse('https://api.telegram.org/bot$token/sendMessage');
+  final getUpdatesUrl = Uri.parse('https://api.telegram.org/bot$token/getUpdates');
+
+  final response = await http.get(getUpdatesUrl);
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> updatesData = jsonDecode(response.body);
+
+    if (updatesData.containsKey('result') && updatesData['result'] is List) {
+      final List<dynamic> result = updatesData['result'];
+
+      if (result.isNotEmpty) {
+
+        for (var updatesData in result) {
+          if (updatesData.containsKey('message')){
+
+            final message = updatesData['message'];
+            final chatId = message['chat']['id'];
+            final text = message['text'];
 
 
 
 
+            if (text == '/start') {
+                final response = await http.post(sendMessageUrl, body:{
+                  'chat_id' : chatId.toString(),
+                  'text' : 'Привет ты нажал старт бота.'
+                });
+              if (response.statusCode == 200) {
+                print ('Message delivered');
+              } else {
+                print ('error, ${response.statusCode}');
 
+              }
+            }
+          }
 
+        }
+      }
 
-   if (sendMessage.statusCode == 200) {
-      print('message  delivered');
-      
-   }
-     else { 
-      print ('no');
-     }
-
-     if (getUpdates.statusCode == 200) {
-          final dynamic data = jsonEncode(getUpdates.body);
-          
-            final message = data['message'];
-            final text = data['text'];
-        await http.post(urlSendMessage, body: {
-          'chat_id' : '612763187',
-          'text' : text,
-        });
-          
     }
+  }
+
 
 
 }
