6609936fbf8d47ff13b9ab3aead8067bca38d5db
psyhh <abarabash.it@gmail.com>
2024-10-03 01:23:55 +0300

    catBot v1.0

--- dart_learning/bin/dart_learning.dart
+++ dart_learning/bin/dart_learning.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http; // http request and responses
 import 'dart:convert'; // json
 
 const String token = '7331024389:AAEQENPYA49rzDZhZITWkmOTLH-9yUeUv7o';
-
+// Функция отправки сообщения на вход принимает кому и сам текст отправки
 Future sendMessage(int chatId, String text) async {
   final sendMessageUrl =
       Uri.parse('https://api.telegram.org/bot$token/sendMessage');
@@ -12,13 +12,24 @@ Future sendMessage(int chatId, String text) async {
     'text': text,
   });
   if (response.statusCode == 200) {
-    print('message sent');
+    print('Сообщение отправлено(функция sendMessage)');
   } else {
     print(
-        'message didnt delivered (error), status code: ${response.statusCode}');
+        'Сообщение не отправлено, status code: ${response.statusCode}');
   }
 }
+Future<String> catApi() async{
+  final String apiKey = 'live_t0hv3sUJS31fd7UYQpxbVM0Y8WnSaSDq1dLhR3aj5JObLvLhFVMXM1UyeSjR6XOL';
+  final catApiUrl = Uri.parse('https://api.thecatapi.com/v1/images/search');
+  final response = await http.get(catApiUrl);
+  if (response.statusCode == 200) {
+    final List<dynamic> catData = jsonDecode(response.body);
+    final catImageUrl = catData[0]['url'];
+    return catImageUrl;
+  } else { return '';}
 
+}
+// Функция API кітиків отдает случайный факт в виде строки
 Future<String> getCatFact() async {
   final catUrl = Uri.parse('https://catfact.ninja/fact');
   final response = await http.get(catUrl);
@@ -27,17 +38,14 @@ Future<String> getCatFact() async {
     final randomCatFact = jsonDecode(response.body);
     print('Факт о котиках успешно отправлен');
     return randomCatFact['fact'];
-    
-  } else 
-  {
+  } else {
     print('Не получилось подтянуть факт о котиках');
     return '';
   }
 }
 
 Future<void> sendMessageWithButtons(int chatId) async {
-  final sendMessageUrl =
-      Uri.parse('https://api.telegram.org/bot$token/sendMessage');
+  final sendMessageUrl = Uri.parse('https://api.telegram.org/bot$token/sendMessage');
 
   final inlineKeyboard = {
     'inline_keyboard': [
@@ -45,14 +53,14 @@ Future<void> sendMessageWithButtons(int chatId) async {
         {'text': 'Перша піпка', 'callback_data': 'button_1'}
       ],
       [
-        {'text': 'Друга піпка', 'callback_data': 'button_2'}
+        {'text': 'Взнати цікавий факт про кітиків', 'callback_data': 'button_2'}
       ]
     ]
   };
 
   final response = await http.post(sendMessageUrl, body: {
     'chat_id': chatId.toString(),
-    'text': 'with buttons',
+    'text': 'Повідомлення з піпками',
     'reply_markup': jsonEncode(inlineKeyboard),
   });
 
@@ -66,8 +74,6 @@ Future<void> sendMessageWithButtons(int chatId) async {
 void main() async {
   int offset = 0;
   while (true) {
-    final sendMessageUrl =
-        Uri.parse('https://api.telegram.org/bot$token/sendMessage');
     final getUpdatesUrl = Uri.parse(
         'https://api.telegram.org/bot$token/getUpdates?offset=$offset');
 
@@ -80,35 +86,49 @@ void main() async {
         final List<dynamic> result = updatesData['result'];
 
         if (result.isNotEmpty) {
-          for (var updatesData in result) {
-            if (updatesData.containsKey('message')) {
-              final message = updatesData['message'];
+          
+          for (var update in result) {
+            // Перевіряємо чи є callback_query
+            if (update.containsKey('callback_query')) {
+              final callbackQuery = update['callback_query'];
+              final chatId = callbackQuery['message']['chat']['id'];
+              final callbackData = callbackQuery['data'];
+              print(callbackData);
+
+              // Обробка callback_data
+              if (callbackData == 'button_1') {
+                await sendMessage(chatId, 'Ви натиснули першу кнопку');
+              } else if (callbackData == 'button_2') {
+                final String catFact = await getCatFact(); // Чекаємо на факт про кота
+                await sendMessage(chatId, catFact);
+              }
+
+              // Оновлюємо offset після обробки callback_query
+              offset = update['update_id'] + 1;
+            }
+
+            // Перевіряємо чи є звичайне повідомлення
+            if (update.containsKey('message')) {
+              final message = update['message'];
               final chatId = message['chat']['id'];
               final text = message['text'];
-
-              if (updatesData.containsKey('callback_query')) {
-                final callbackQuery = updatesData['callback_query'];
-                final chatId = callbackQuery['message']['chat']['id'];
-                final callbackData = callbackQuery['data'];
-
-                // Обработка действий в зависимости от callback_data
-                if (callbackData == 'button_1') {
-                  await sendMessage(chatId, 'pl');
-                } else if (callbackData == 'button_2') {
-                  await sendMessage(chatId, getCatFact().toString());
-                }
-              }
 
               if (text == '/options') {
                 sendMessageWithButtons(chatId);
               }
-              // Обновляем offset, чтобы не получать одно и то же сообщение
-              offset = updatesData['update_id'] + 1;
+              if (text == '/start') {
+                sendMessage(chatId, 'Вітаю в чат боті кітиків!');
+                sendMessage(chatId, await catApi());
+              }
+
+              // Оновлюємо offset після обробки повідомлення
+              offset = update['update_id'] + 1;
             }
           }
         }
       }
     }
+
     await Future.delayed(Duration(seconds: 3));
   }
 }
